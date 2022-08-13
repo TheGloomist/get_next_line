3c5c011e6870d195b0c26cf51ee06de7c1fccfbf
TheGloomist <woodchips.picture@gmail.com>
2022-08-13 12:24:51 +0200

    Quick commit to not carry the laptop everywhere uwu

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,18 @@
 #include "get_next_line.h"
 
+int find_newline(char *line)
+{
+    int i;
+
+    i = 0;
+    while (line[i] != '\0' && line[i] != '\n')
+        i++;
+    if (line[i] == '\n')
+        return(i);
+    else
+        return(-1);
+}
+
 char    *get_next_line(int fd)
 {
     char *line;
